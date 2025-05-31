27d965ac787f4c303eac2fc8d1f20199f93d0fbf
Ramaa-Devs <diwan.123140116@student.itera.ac.id>
2025-05-31 12:56:22 +0700

    Add files via upload

---
+++ tubes1-IF2211-bot-starter-pack-1.0.1/run-bots.bat
@@ -0,0 +1,4 @@
+@echo off
+start cmd /c "python main.py --logic mysmartbot --email=MySmartBot@email.com --name=MySmartBot --password=123456 --team etimo"
+start cmd /c "python main.py --logic waktu --email=Waktu@email.com --name=Waktu1 --password=123456 --team etimo"
+
