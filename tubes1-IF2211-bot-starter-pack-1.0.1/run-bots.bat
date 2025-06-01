1822f14ad8af88b450bd23542d6aca89b88fb57f
Ramaa-Devs <diwan.123140116@student.itera.ac.id>
2025-06-01 15:39:48 +0700

    Update run-bots.bat

--- tubes1-IF2211-bot-starter-pack-1.0.1/run-bots.bat
+++ tubes1-IF2211-bot-starter-pack-1.0.1/run-bots.bat
@@ -1,4 +1,6 @@
 @echo off
-start cmd /c "python main.py --logic mysmartbot --email=MySmartBot@email.com --name=MySmartBot --password=123456 --team etimo"
-start cmd /c "python main.py --logic waktu --email=Waktu@email.com --name=Waktu1 --password=123456 --team etimo"
 
+start cmd /c "python main.py --logic cep --email=cep@example.com --name=cep --password=123456 --team etimo"
+start cmd /c "python main.py --logic tw --email=tw@example.com --name=tw --password=123456 --team etimo"
+start cmd /c "python main.py --logic ra --email=ra@example.com --name=ra --password=123456 --team etimo"
+start cmd /c "python main.py --logic vtd --email=vtd@example.com --name=vtd --password=123456 --team etimo"
