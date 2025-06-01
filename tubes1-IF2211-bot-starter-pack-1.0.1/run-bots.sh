a5ae144d673754acaae81012839c30fcea76ec5d
Ramaa-Devs <diwan.123140116@student.itera.ac.id>
2025-06-01 21:00:26 +0700

    Update run-bots.sh

--- tubes1-IF2211-bot-starter-pack-1.0.1/run-bots.sh
+++ tubes1-IF2211-bot-starter-pack-1.0.1/run-bots.sh
@@ -1,6 +1,4 @@
-@echo off
-
-start cmd /c "python main.py --logic cep --email=cep@example.com --name=cep --password=123456 --team etimo"
-start cmd /c "python main.py --logic tw --email=tw@example.com --name=tw --password=123456 --team etimo"
-start cmd /c "python main.py --logic ra --email=ra@example.com --name=ra --password=123456 --team etimo"
-start cmd /c "python main.py --logic vtd --email=vtd@example.com --name=vtd --password=123456 --team etimo"
+python main.py --logic cep --email=cep@example.com --name=cep --password=123456 --team etimo &
+python main.py --logic tw --email=tw@example.com --name=tw --password=123456 --team etimo &
+python main.py --logic ra --email=ra@example.com --name=ra --password=123456 --team etimo &
+python main.py --logic vtd --email=vtd@example.com --name=vtd --password=123456 --team etimo &
